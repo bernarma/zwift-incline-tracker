cf5af2454d690bbded4b600448f5e69ff42aff57
Mark Bernardinis <mark.bernardinis@gmail.com>
2022-04-05 07:46:29 +1000

    Updated packages.

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
-let yaml = require('js-yaml'),
-	fs = require('fs'),
-	path = require('path');
+let yaml = require("js-yaml"),
+  fs = require("fs"),
+  path = require("path");
 
 /*
  Public API.
@@ -18,52 +18,50 @@ exports.save = save;
  Initialization.
  */
 let minimumVersion = 1,
-	ref = path.join(__dirname, '..', 'settings.conf');
+  ref = path.join(__dirname, "..", "settings.conf");
 
 /*
  Implementation.
  */
 
 function load() {
-	if (!fs.existsSync(ref)) {
-		return false;
-	}
-	try {
-		let settings = yaml.safeLoad(fs.readFileSync(ref, 'UTF-8'));
-		if (settings) {
-			if (settings.version && settings.version < minimumVersion) {
-				console.log('New settings have been added!');
-				settings = null;
-			}
-			else {
-				fromJSON(settings);
-			}
-		}
-		return !!settings;
-	}
-	catch (err) {
-		return false;
-	}
+  if (!fs.existsSync(ref)) {
+    return false;
+  }
+  try {
+    let settings = yaml.safeLoad(fs.readFileSync(ref, "UTF-8"));
+    if (settings) {
+      if (settings.version && settings.version < minimumVersion) {
+        console.log("New settings have been added!");
+        settings = null;
+      } else {
+        fromJSON(settings);
+      }
+    }
+    return !!settings;
+  } catch (err) {
+    return false;
+  }
 }
 
 function toJSON() {
-	let settings = {};
-	for (let key in exports) {
-		if (exports.hasOwnProperty(key) && typeof exports[key] !== 'function') {
-			settings[key] = exports[key];
-		}
-	}
-	return settings;
+  let settings = {};
+  for (let key in exports) {
+    if (exports.hasOwnProperty(key) && typeof exports[key] !== "function") {
+      settings[key] = exports[key];
+    }
+  }
+  return settings;
 }
 
 function fromJSON(json) {
-	for (let key in json) {
-		if (json.hasOwnProperty(key)) {
-			exports[key] = json[key];
-		}
-	}
+  for (let key in json) {
+    if (json.hasOwnProperty(key)) {
+      exports[key] = json[key];
+    }
+  }
 }
 
 function save() {
-	fs.writeFile(ref, yaml.safeDump(toJSON()), 'UTF-8', () => {});
+  fs.writeFile(ref, yaml.dump(toJSON()), "UTF-8", () => {});
 }
